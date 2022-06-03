ba1065f508a566e039718bc5a6a9e302d09ab71e
Gabriel Helms <gabegbh@gmail.com>
2022-06-03 13:18:19 -0500

    Generate feedback in form of loss and accuracy graphs for test and validation sets

--- training.py
+++ training.py
@@ -2,17 +2,19 @@ import keras,os
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
 from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.optimizers import Adam
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 def create_network():
     network = Sequential()
-    network.add(Conv2D(input_shape=(32,32,3),filters=12,kernel_size=(3,3), activation="relu"))
-    network.add(Conv2D(filters=12,kernel_size=(3,3), activation="relu"))
+    network.add(Conv2D(input_shape=(32,32,3),filters=24,kernel_size=(3,3), activation="relu"))
+    network.add(Conv2D(filters=24,kernel_size=(3,3), activation="relu"))
     network.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
-    network.add(Conv2D(filters=24, kernel_size=(3,3),  activation="relu"))
-    network.add(Conv2D(filters=24, kernel_size=(3,3),  activation="relu"))
+    network.add(Conv2D(filters=48, kernel_size=(3,3),  activation="relu"))
+    network.add(Conv2D(filters=48, kernel_size=(3,3),  activation="relu"))
     network.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     # network.add(Conv2D(filters=256, kernel_size=(3,3),  activation="relu"))
     # network.add(Conv2D(filters=256, kernel_size=(3,3),  activation="relu"))
@@ -33,14 +35,33 @@ def create_network():
     
     return network
 
+def display_metrics(history):
+    """ plot loss and accuracy from keras history object """
+    f, ax = plt.subplots(1, 2, figsize=(15, 5))
+    ax[0].plot(history.history['loss'], linewidth=3)
+    ax[0].plot(history.history['val_loss'], linewidth=3)
+    ax[0].set_title('Loss', fontsize=16)
+    ax[0].set_ylabel('Loss', fontsize=16)
+    ax[0].set_xlabel('Epoch', fontsize=16)
+    ax[0].legend(['train loss', 'val loss'], loc='upper right')
+    ax[1].plot(history.history['accuracy'], linewidth=3)
+    ax[1].plot(history.history['val_accuracy'], linewidth=3)
+    ax[1].set_title('Accuracy', fontsize=16)
+    ax[1].set_ylabel('Accuracy', fontsize=16)
+    ax[1].set_xlabel('Epoch', fontsize=16)
+    ax[1].legend(['train acc', 'val acc'], loc='upper left')
+    plt.show()
+
 if __name__ == '__main__':
     training_data_gen = ImageDataGenerator(validation_split=0.2)
-    training_data = training_data_gen.flow_from_directory(directory="GTSRB/Final_Training",target_size=(32,32), subset='training')
-    validation_data = training_data_gen.flow_from_directory(directory="GTSRB/Final_Training",target_size=(32,32), subset='validation')
+    training_data = training_data_gen.flow_from_directory(directory="GTSRB/Final_Training/Images",target_size=(32,32), subset='training')
+    validation_data = training_data_gen.flow_from_directory(directory="GTSRB/Final_Training/Images",target_size=(32,32), subset='validation')
 
-    opt = Adam(lr=0.01)
+    opt = Adam(lr=0.0005)
 
     model = create_network()
-    model.compile(optimizer=opt, loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
-
-    model.summary()
+    model.compile(optimizer=opt, loss=keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+    history = model.fit(x=training_data, 
+                        epochs=20, 
+                        validation_data=validation_data)
+    display_metrics(history)
