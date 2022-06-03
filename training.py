a480a3967187ee8692e305af47171fb799000abb
Gabriel Helms <gabegbh@gmail.com>
2022-06-03 11:57:39 -0500

    Initial commit with training set

---
+++ training.py
@@ -0,0 +1,5 @@
+import keras,os
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
